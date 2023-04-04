8fee6b45c695fb872d1499b33fb3eb8e923fa774
Brooks McKinley <31021010+brooksmckinley@users.noreply.github.com>
2023-04-04 18:54:40 -0400

    Build basic login page structure

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,36 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import InputField from "../components/InputField";
+
 function Login() {
-    return <p>This is the login page.</p>
+    // Form field data
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // Insert code to submit the form
+    function submitForm() {
+        console.log("Submit button clicked.");
+    }
+    
+    return (
+        <>
+            <h1>Weather App</h1>
+            <div>
+                <h2>Sign-In</h2>
+                <p>Fill out this form.</p>
+                <div>
+                    <InputField type="text" placeholder="First Name" onChange={(e) => { setFirstName(e.target.value) }} />
+                    <InputField type="text" placeholder="Last Name" onChange={(e) => { setLastName(e.target.value) }} />
+                </div>
+                <InputField type="email" placeholder="E-Mail Address" iconName="email.svg" onChange={(e) => { setEmail(e.target.value) } } />
+                <InputField type="password" placeholder="Password" iconName="password.svg" onChange={(e) => { setPassword(e.target.value) } } />
+                <Link to="/register">Don't have an account? Click here to register.</Link>
+                <button onClick={submitForm}>Sign-In</button>
+            </div>
+        </>
+    );
 }
 
 export default Login;
