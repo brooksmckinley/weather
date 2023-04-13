4d89a10adba26c1588041e812ba1a0d0871be27e
Brooks McKinley <31021010+brooksmckinley@users.noreply.github.com>
2023-04-13 16:45:11 -0400

    Fix up font sizes and weight for CityCard

--- frontend/src/components/CityCard.js
+++ frontend/src/components/CityCard.js
@@ -4,14 +4,14 @@ function CityCard({cityName, highTemp, lowTemp, currentTemp}) {
 
     return <div className="cityCard">
         <div>
-            <div>
+            <div className="cityName">
                 {cityName}
             </div>
-            <div>
+            <div className="highLowTemp">
                 H: {highTemp}°F L: {lowTemp}°F
             </div>
         </div>
-        <div>
+        <div className="currentTemp">
             {currentTemp}°
         </div>
     </div>;
