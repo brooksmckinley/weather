939a66256ab347a49a32471bbdd9c12d4c6b9082
Brooks McKinley <31021010+brooksmckinley@users.noreply.github.com>
2023-04-18 18:41:31 -0400

    half-connect login page to backend

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -9,8 +9,23 @@ function Login() {
     const [password, setPassword] = useState("");
 
     // Insert code to submit the form
-    function submitForm() {
-        console.log("Submit button clicked.");
+    async function submitForm() {
+        let body = JSON.stringify({email, password});
+        let request = await fetch("http://localhost:5000/login/login", {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+              "Content-Type": "application/json",
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body, // body data type must match "Content-Type" header
+          });
+
+        let data = await request.json();
+        alert(data.msg);
     }
     
     return (
