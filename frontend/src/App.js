fb033a2374098b3d31bc156cae724472bd343b84
Ruth Pearl <ruth.j.pearl@gmail.com>
2023-04-20 12:50:07 -0400

    Preliminary forecast page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,7 +40,7 @@ function App() {
         <Route index element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/forecast" element={<Forecast />} />
+        <Route path="/forecast" element={<Forecast cityName="Orlando" currentTemp={77}/>} />
         <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
