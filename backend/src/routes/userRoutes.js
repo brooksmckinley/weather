ea81e00c9103a060689e83b731fd1ded4f25aa21
ethandrzb <ethandrzb@gmail.com>
2023-04-20 18:02:40 -0400

    Start of editUser

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -17,10 +17,45 @@ router.get('/', (req, res) =>
     res.json({msg: "Hello from Express!"});
 });
 
+// TODO: Finish this function
 // Update non-location info for user
-router.patch('/', (req, res) =>
+router.patch('/', async (req, res) =>
 {
-    res.json({msg: "Update non-location info for user"}); 
+    // res.json({msg: "Update non-location info for user"});
+
+    // If this function returns a non-null value, you can safely assume
+    // that the user is logged in.
+    const userID = getUserID(req);
+
+    if(userID != null)
+    {
+        // Remove empty fields from request body
+        for(var key of Object.keys(req.body))
+        {
+            console.log(key + ": " + req.body[key]);
+            if(!req.body[key] || key === "password")
+            {
+                delete req.body[key];
+            }
+        }
+
+        console.log(req.body);
+        if(Object.keys(req.body).length === 0)
+        {
+            res.status(400);
+            res.json({msg: "Invalid request: All fields empty or missing"});
+        }
+        else
+        {
+            const user = await User.findOneAndUpdate({_id: userID}, req.body, {new: true});
+            res.json({msg: "Hello " + user.firstName + " " + user.lastName + "!"});
+        }
+    }
+    else
+    {
+        res.status(401);
+        res.json({msg: "Unauthorized"});
+    }
 });
 
 // Delete user from database
