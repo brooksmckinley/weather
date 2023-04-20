5d6d71f5c19d631269b6b28367a5318fd0d083ac
ethandrzb <ethandrzb@gmail.com>
2023-04-19 22:04:46 -0400

    Delete user route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose'),
+                Schema = mongoose.Schema,
+                bcrypt = require('bcrypt');
+
+require('dotenv').config();
+
+const {getUserID} = require('../utils.js');
+
+const User = require("../schemas/User");
 const router = express.Router();
 
 module.exports = router;
@@ -15,7 +24,25 @@ router.patch('/', (req, res) =>
 });
 
 // Delete user from database
-router.delete('/', (req, res) =>
+router.delete('/', async (req, res) =>
 {
-    res.json({msg: "Delete user"}); 
+    const userID = getUserID(req);
+
+    if(userID != null)
+    {
+        console.log(userID);
+
+        const user = await User.findOneAndDelete({_id: userID});
+        console.log("Deleted " + user);
+
+        // Remove cookie for non-existent user
+        res.clearCookie('jwt');
+
+        res.json({msg: "Deleted " + userID});
+    }
+    else
+    {
+        res.status(401);
+        res.json({msg: "Unauthorized"});
+    }
 });
