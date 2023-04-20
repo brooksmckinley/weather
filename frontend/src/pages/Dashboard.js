24570055c1f531b4fdbd8e3f06cd7fffffc6a878
Brooks McKinley <31021010+brooksmckinley@users.noreply.github.com>
2023-04-20 12:43:08 -0400

    Build out dashboard with basic test data

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,5 +1,57 @@
+import { useState } from "react";
+
+import CityCard from "../components/CityCard";
+import "./Dashboard.css";
+
 function Dashboard() {
-    return <p>This is the user dashboard page.</p>
+    // Dummy state
+    const TEST_CITIES = [
+        {
+            cityName: "Orlando",
+            highTemp: 80,
+            lowTemp: 60,
+            currentTemp: 77,
+        },
+        {
+            cityName: "Tampa",
+            highTemp: 80,
+            lowTemp: 60,
+            currentTemp: 77,
+        },
+        {
+            cityName: "Phoenix",
+            highTemp: 80,
+            lowTemp: 60,
+            currentTemp: 77,
+        },
+        {
+            cityName: "New York",
+            highTemp: 80,
+            lowTemp: 60,
+            currentTemp: 77,
+        },
+    ];
+
+    const [cities, setCities] = useState(TEST_CITIES);
+
+    function renderCities() {
+        return cities.map(function (city) {
+            return <CityCard
+             cityName={city.cityName} 
+             highTemp={city.highTemp} 
+             lowTemp={city.lowTemp} 
+             currentTemp={city.currentTemp}
+             key={city.cityName}
+            />
+        })
+    }
+    
+    return <div>
+        <h1>Welcome, John</h1>
+        <div className="cardContainer">
+            { renderCities() }
+        </div>
+    </div>
 }
 
 export default Dashboard;
