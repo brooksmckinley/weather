5c783a0d39b2030e21f2d8babc285cab67621bf7
Brooks McKinley <brooksmckinley@yahoo.com>
2023-04-22 12:48:15 -0400

    Switch from using useEffect to useMemo for performance

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,45 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState, useMemo } from "react";
 import CityCard from "../components/CityCard";
 import "./Dashboard.css";
 import Search from "../components/search";
 import { Link } from "react-router-dom";
 
+// Dummy state
+// TODO: Replace all instances of TEST_CITIES with a real array of cities obtained from the API.
+const TEST_CITIES = [
+    {
+        cityName: "Orlando",
+        highTemp: 80,
+        lowTemp: 60,
+        currentTemp: 77,
+    },
+    {
+        cityName: "Tampa",
+        highTemp: 80,
+        lowTemp: 60,
+        currentTemp: 77,
+    },
+    {
+        cityName: "Phoenix",
+        highTemp: 80,
+        lowTemp: 60,
+        currentTemp: 77,
+    },
+    {
+        cityName: "New York",
+        highTemp: 80,
+        lowTemp: 60,
+        currentTemp: 77,
+    },
+];
+
 function Dashboard() {
-    // Dummy state
-    // TODO: Replace all instances of TEST_CITIES with a real array of cities obtained from the API.
-    const TEST_CITIES = [
-        {
-            cityName: "Orlando",
-            highTemp: 80,
-            lowTemp: 60,
-            currentTemp: 77,
-        },
-        {
-            cityName: "Tampa",
-            highTemp: 80,
-            lowTemp: 60,
-            currentTemp: 77,
-        },
-        {
-            cityName: "Phoenix",
-            highTemp: 80,
-            lowTemp: 60,
-            currentTemp: 77,
-        },
-        {
-            cityName: "New York",
-            highTemp: 80,
-            lowTemp: 60,
-            currentTemp: 77,
-        },
-    ];
+    
 
     const [text, searchCity] = useState("");
     const [cities, setCities] = useState(TEST_CITIES);
     const [firstName, setFirstName] = useState("Loading...");
 
     // Filter the list of cities when the search text changes using a case-insensitive search.
-    useEffect(() => setCities(TEST_CITIES.filter(city => city.cityName.toLowerCase().includes(text.toLowerCase()))), [text]);
+    useMemo(() => setCities(TEST_CITIES.filter(city => city.cityName.toLowerCase().includes(text.toLowerCase()))), [text]);
 
     function renderCities() {
         return cities.map(function (city) {
