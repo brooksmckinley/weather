14b2c832eb3f92456dd9055c17b09351c819471d
Brooks McKinley <brooksmckinley@yahoo.com>
2023-04-24 15:41:00 -0400

    Partially implement city loading

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,8 +1,9 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import CityCard from "../components/CityCard";
 import "./Dashboard.css";
 import Search from "../components/search";
 import { Link } from "react-router-dom";
+import ENVIRONMENT from "../utils/environment";
 
 // Dummy state
 // TODO: Replace all instances of TEST_CITIES with a real array of cities obtained from the API.
@@ -35,19 +36,67 @@ const TEST_CITIES = [
 
 function Dashboard() {
     const [text, searchCity] = useState("");
+    const [cities, setCities] = useState([]);
     const [firstName, setFirstName] = useState("Loading...");
 
+    // Load in user data and their locations when the page first loads.
+    useEffect(() => {
+        let active = true;
+
+        const loadUserData = async () => {
+            // Load in user info and basic location information
+            const userRequest = await fetch(`${ENVIRONMENT.BACKEND_URL}/user`);
+            const user = await userRequest.json();
+
+            const forecasts = [];
+            // Load in each of the user's locations in parallel
+            await Promise.all(user.locations.map(async (city) => {
+                const forecastRequest = await fetch(`${ENVIRONMENT.BACKEND_URL}/weather/DailyForecast`, {
+                    method: "POST", // *GET, POST, PUT, DELETE, etc.
+                    mode: "cors", // no-cors, *cors, same-origin
+                    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                    credentials: "same-origin", // include, *same-origin, omit
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    redirect: "follow", // manual, *follow, error
+                    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                    body: { Key: city.locationKey }
+                });
+                const forecast = await forecastRequest.json();
+                forecasts[city.locationKey] = forecast;
+            }));
+
+            // Place all of the loaded information into state if the component that called this effect hasn't been removed.
+            if (active) {
+                setCities(user.locations.map((city) => {
+                    return {
+                        cityName: city.city,
+                        highTemp: forecasts[city.locationKey].DailyForecasts[0].Temperature.Maximum.Value,
+                        lowTemp: forecasts[city.locationKey].DailyForecasts[0].Temperature.Minimum.Value,
+                        currentTemp: 30,
+                        key: city.locationKey,
+                    };
+                }));
+                setFirstName(user.firstName);
+            }
+        };
+        loadUserData();
+
+        return () => active = false;
+    }, []);
+
     // Filter the list of cities when the search text changes using a case-insensitive search.
-    const cities = useMemo(() => TEST_CITIES.filter(city => city.cityName.toLowerCase().includes(text.toLowerCase())), [text]);
+    const filteredCities = useMemo(() => TEST_CITIES.filter(city => city.cityName.toLowerCase().includes(text.toLowerCase())), [text]);
 
     function renderCities() {
-        return cities.map(function (city) {
+        return filteredCities.map(function (city) {
             return <CityCard
              cityName={city.cityName} 
              highTemp={city.highTemp} 
              lowTemp={city.lowTemp} 
              currentTemp={city.currentTemp}
-             key={city.cityName}
+             key={city.key}
             />
         })
     }
